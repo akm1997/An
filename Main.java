8f1ce920c06a7b14cef3fdfec38c704396ffcf76
akm1997 <57319546+akm1997@users.noreply.github.com>
2019-11-03 22:22:27 +0900

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,84 +1,111 @@
-import java.util.Random;
-import java.util.Scanner;
+package practieJava;
+
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String args[]) {
+		ArrayList<Student> list = new ArrayList<Student>();
+		 ClassTotalComparator obj = new ClassTotalComparator();
+		// ClassStudentNo obj1 = new ClassStudentNo();
 
-		Scanner scanner = new Scanner(System.in);
+		list.add(new Student("남궁성", 3, 2, 100, 100, 100));
+		list.add(new Student("염따", 8, 2, 100, 100, 100));
+		list.add(new Student("키드밀리", 8, 2, 100, 100, 90));
+		list.add(new Student("안기민", 5, 3, 90, 90, 90));
+		list.add(new Student("장광남", 5, 3, 90, 90, 95));
+		list.add(new Student("장광남", 5, 3, 90, 980, 95));
+		list.add(new Student("빈지노", 2, 1, 50, 50, 51));
+		list.add(new Student("더콰이엇", 2, 1, 50, 50, 52));
+		list.add(new Student("창모", 1, 2, 5, 10, 15));
+		list.add(new Student("저스디스", 4, 2, 5, 10, 15));
+		list.add(new Student("오왼", 3, 3, 20, 40, 15));
 
-		String[] strArr = { "CHANGE", "LOVE", "HOPE", "VIEW" };
+		//calculateSchoolRank(list);
+		//calculateClassRank(list);
+		//Collections.sort(list,obj);
+		calculateSchoolRank(list);
+		calculateClassRank(list);
+		printList(list);
 
-		while (true) {
-			String answer = getAnswer(strArr);
-			String question = getScrambledWord(answer);
-			char hint[] = new char[answer.length()];
+	}
 
-			for (int i = 0; i < hint.length; i++) {
-				hint[i] = '_';
+	public static void calculateSchoolRank(ArrayList<Student> list) {
+		Collections.sort(list);
+		int prevRank = 1;
+		double prevTotal = 0;
+		int length = list.size();
+		for (int i = 0; i < length; i++) {
+			if (i == 0) {
+				prevTotal = list.get(0).Total;
+				list.get(i).schoolRank = prevRank;
+				prevRank = list.get(i).schoolRank;
 			}
+			if (i > 0) {
+				if (prevTotal == list.get(i).getTotal()) {
+					list.get(i).schoolRank = prevRank;
 
-			System.out.println("Question:" + question);
-			System.out.println("Your answer is:");
+					prevRank = list.get(i).schoolRank;
+					prevTotal = list.get(i).getTotal();
 
-			String UserInput = scanner.nextLine();
-			if (UserInput.equals(answer)) {
-				System.out.println("정답!");
-			} else if (UserInput.equals("q") || UserInput.equals("Q")) {
-				break;
-			} else {
-				while (!UserInput.equals(answer)) {                       //답이 틀릴경우 정답이 될 때까지 반복한다.
-
-					System.out.println(UserInput + "은 정답이 아닙니다. 다시 입력하시오");
-					System.out.println("Hint=" + getHint(answer, hint));
-					System.out.println("Your answer is:");
-					UserInput = scanner.nextLine();
-
+				} else if (list.get(i).getTotal() < prevTotal) {
+					prevRank++;
+					list.get(i).schoolRank = prevRank;
+					prevTotal = list.get(i).getTotal();
 				}
 			}
 		}
 
 	}
 
-	public static String getAnswer(String[] strArr) {         // 정답 배열에서 랜덤으로 정답을 선택하는 메소드
-		String answer;
-		int i = (int) (Math.random() * strArr.length);
-		answer = strArr[i];
-
-		return answer;
-	}
-
-	public static String getScrambledWord(String str) {         //정답 단어를 섞어주는 메소드
-		char[] arr = str.toCharArray();
-
-		for (int i = 0; i < str.length(); i++) {
-			int idx = (int) Math.random() * str.length();
-			char tmp = arr[i];
-			arr[i] = arr[idx];
-			arr[idx] = tmp;
-
-		}
-		return new String(arr);
-	}
-
-
-
 	
-	public static String getHint(String answer, char[] hint) {      //힌트를 주는 메소드
-		int count = 0;                    //'_'의 갯수
+	
+	
+	
+	
+	public static void calculateClassRank(ArrayList<Student> list) {
+		Collections.sort(list, new ClassTotalComparator());
 
-		for (int i = 0; i < hint.length; i++) {   
-			if (hint[i] == '_')
-				++count;
+		int length = list.size();
+		int prevClassNo = 0;
+		int prevClassRank = 1;
+		double prevTotal = 0;
+
+		for (int i = 0; i < length; i++) {
+
+			Student s = list.get(i);
+
+			if (s.classNo != prevClassNo) {
+				s.classRank = 1;
+
+			}
+			if (s.classNo == prevClassNo) {
+				if (s.Total == prevTotal) {
+					s.setClassRank(prevClassRank);
+
+				}
+				if (s.Total != prevTotal) {
+
+					s.classRank = ++prevClassRank;
+				}
+
+			}
+			prevClassNo = s.classNo;
+			prevClassRank = s.classRank;
+			prevTotal = s.Total;
+
 		}
 
-		if (count >= 2) {      //'_'의 갯수가 2개 이상일 경우에만 힌트 글자를 추가한다   정답을 다 안알려주기 위함임
-			
-
-			int idx = (int) (Math.random() * answer.length());
-			hint[idx] = answer.charAt(idx);
-
-		}
-		return new String(hint);
 	}
+
+	public static void printList(ArrayList<Student> list) {
+		System.out.println("이름\t반\t번호\t국어\t수학\t영어\t총점\t등수\t반등수");
+		System.out.println("======================================================");
+		for (Student s : list) {
+			System.out.println(s);
+		}
+		System.out.println("======================================================");
+	}
+
 }
